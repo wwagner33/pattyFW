c14e3e72492eb5ad7bf27e03b0247f372872e1ae
patriciaspaula <patricia@virtual.ufc.br>
2017-12-28 18:40:03 -0300

    alterando alma server

--- alma/server.js
+++ alma/server.js
@@ -2,32 +2,24 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
-  Activity = require('./api/models/activityModel'), //created model loading here
-  Laboratory = require('./api/models/laboratoryModel'),
-  UserContext = require('./api/models/userContextModel'),
-  UserInteraction = require('./api/models/userInteractionModel'),
-  User = require('./api/models/userModel'),
-  WidgetContext = require('./api/models/widgetContextModel'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+
+  Alma = require('./class/alma'),
+  Laves = require('./class/laves'),
+  Smil = require('./class/smil'),
+
+  Activity = require('./models/activityModel'),
+  Laboratory = require('./models/laboratoryModel'),
+  UserContext = require('./models/userContextModel'),
+  UserInteraction = require('./models/userInteractionModel'),
+  User = require('./models/userModel'),
+  WidgetContext = require('./models/widgetContextModel');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// css and images
+// css e imagens
 app.use(express.static(__dirname + '/public'));
-
-// use res.render to load up an ejs view file
-app.get('/', function(req, res) {
-  res.render('pages/index'); // index page
-});
-
-app.get('/list_activity', function(req, res) {
-  //Activity.find({'local':''}).exec(function(err, activities) {
-  Activity.find({}).exec(function(err, activities) {
-    if (err) throw err;
-    res.render('pages/list_activity', {'activities':activities});
-  });
-});
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
@@ -36,9 +28,12 @@ mongoose.connect('mongodb://localhost/ulabpa');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var routes = require('./api/routes/allRoutes'); //importing route
-routes(app); //register the route
+var routes = require('./apiRoutes'); //importando rotas da api
+routes(app); //registrando rotas
+
+routes = require('./appLabRoutes');
+routes(app);
 
 app.listen(port);
 
-console.log('Started on: ' + port);
+console.log('Alma is alive - port: ' + port);

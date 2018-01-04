451f36ad01c6f2212a3ee7220407b568295f702a
patriciaspaula <patricia@virtual.ufc.br>
2018-01-04 19:03:09 -0300

    arquivos fora do git

---
+++ alma/controllers/userController.js
@@ -0,0 +1,21 @@
+'use strict';
+var mongoose = require('mongoose'),
+  User = mongoose.model('Users');
+
+// REMOVER TUDO
+
+exports.read_a_user = function(req, res) {
+  User.findById(req.params.userId, function(err, user) {
+    if (err)
+      res.send(err);
+    res.json(user);
+  });
+};
+
+exports.update_a_user = function(req, res) {
+  User.findOneAndUpdate({_id: req.params.userId}, req.body, {new: true}, function(err, user) {
+    if (err)
+      res.send(err);
+    res.json(user);
+  });
+};

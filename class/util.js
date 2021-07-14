391e1558a0a5a587f82fbcbea31cecd0dd5be239
Wellington Wagner F. Sarmento <wwagner@virtual.ufc.br>
2021-07-14 17:03:34 -0300

    put Logging in test application.

--- class/util.js
+++ class/util.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
@@ -17,16 +19,16 @@ class Logging{
             console.error(err);
         }   
         this.logPath = path.join(__dirname, logDir, 'access.log');
-        this.writeLogStream = fs.createWriteStream(logPath, {flags: 'a+',encoding: 'utf8'}); //Append file if exist or create if not
+        this.writeLogStream = fs.createWriteStream(this.logPath, {flags: 'a+',encoding: 'utf8'}); //Append file if exist or create if not
 
     }
-    createLog(){
-        return morgan('{"remote_addr": ":remote-addr", "remote_user": ":remote-user", "date": ":date[clf]", "method": ":method", "url": ":url", "http_version": ":http-version", "status": ":status", "result_length": ":res[content-length]", "referrer": ":referrer", "user_agent": ":user-agent", "response_time": ":response-time"}', {stream: writeLogStream});
+    createLog(logFields='{"remote_addr": ":remote-addr", "remote_user": ":remote-user", "date": ":date[clf]", "method": ":method", "url": ":url", "http_version": ":http-version", "status": ":status", "result_length": ":res[content-length]", "referrer": ":referrer", "user_agent": ":user-agent", "response_time": ":response-time"}'){
+        return morgan(logFields,{stream: this.writeLogStream});
     }
     clearLog(){
 
     }
 }
 
+module.exports = {Logging};
 
-//module.exports = Logging;

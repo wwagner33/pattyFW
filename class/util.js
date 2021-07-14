91e1d7b16a2560bdf0852faa77311cb9fbab46e3
Wellington Wagner F. Sarmento <wwagner@virtual.ufc.br>
2021-07-14 17:37:27 -0300

    Logging class final version

--- class/util.js
+++ class/util.js
@@ -1,16 +1,26 @@
+/**
+ * @file Alma class
+ * @author Patricia de Sousa Paula <patricia@virtual.ufc.br>
+ * @author Wellington Wagner Ferreira Sarmento <wwagner@virtual.ufc.br>
+ * @version 0.5.0
+ * @license Apache-2.0
+ */
+
 'use strict';
 
 const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
-//const logDir="log";
+
 
 
 class Logging{
-    constructor(logDir="log"){
+    constructor(localDir='./',logDir='log',logFile='access.log'){
+        let logPathPartial = path.join(localDir,logDir);
         try{
-            if(!fs.existsSync(logDir)){ //Test and 
-                fs.mkdirSync(logDir);
+            if(!fs.existsSync(logPathPartial)){ //Test and 
+                
+                fs.mkdirSync(logPathPartial);
                 console.log('Directory created.');
             } else {
                 console.log('Directory already exists.');
@@ -18,7 +28,7 @@ class Logging{
         } catch(err) {
             console.error(err);
         }   
-        this.logPath = path.join(__dirname, logDir, 'access.log');
+        this.logPath = path.join(logPathPartial, logFile);
         this.writeLogStream = fs.createWriteStream(this.logPath, {flags: 'a+',encoding: 'utf8'}); //Append file if exist or create if not
 
     }

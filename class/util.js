43e7d013c98383f695cdc4e7c7af313ec481e2dd
Wellington Wagner F. Sarmento <wwagner@virtual.ufc.br>
2021-07-14 16:26:01 -0300

    Create class Logging in utils

--- class/util.js
+++ class/util.js
@@ -1,27 +1,41 @@
-//const morgan = require('morgan');
+const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
-const logDir="log";
-try{
-    if(!fs.existsSync(logDir)){
-        fs.mkdirSync(logDir);
-        console.log('Directory is created.');
-    } else {
-        console.log('Directory already exists.');
-    }
-} catch(err) {
-    console.error(err);
+//const logDir="log";
+
+
 }
 
 
-let logPath = path.join(__dirname, logDir, 'access.log');
-let writeLogStream = fs.createWriteStream(logPath, {flags: 'a+',encoding: 'utf8'}); //Append file if exist or create if not
+
 
 
     writeLogStream.write('Testing...Testing');
     writeLogStream.end();
 
+class Logging{
+    constructor(logDir="log"){
+        try{
+            if(!fs.existsSync(logDir)){ //Test and 
+                fs.mkdirSync(logDir);
+                console.log('Directory created.');
+            } else {
+                console.log('Directory already exists.');
+            }
+        } catch(err) {
+            console.error(err);
+        }   
+        this.logPath = path.join(__dirname, logDir, 'access.log');
+        this.writeLogStream = fs.createWriteStream(logPath, {flags: 'a+',encoding: 'utf8'}); //Append file if exist or create if not
 
+    }
+    createLog(){
+        return morgan('{"remote_addr": ":remote-addr", "remote_user": ":remote-user", "date": ":date[clf]", "method": ":method", "url": ":url", "http_version": ":http-version", "status": ":status", "result_length": ":res[content-length]", "referrer": ":referrer", "user_agent": ":user-agent", "response_time": ":response-time"}', {stream: writeLogStream});
+    }
+    clearLog(){
+
+    }
+}
 
 
 //module.exports = Logging;
